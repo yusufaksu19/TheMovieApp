39b285690788ebe77a31ce5faa2d5792f597a049
yusufaksu <yusuf.aksu@tsoft.com.tr>
2022-07-20 23:35:47 +0300

    firstcommit

---
+++ lib/views/SearchMoviesPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchMoviesPage extends StatefulWidget {
+  const SearchMoviesPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchMoviesPage> createState() => _SearchMoviesPageState();
+}
+
+class _SearchMoviesPageState extends State<SearchMoviesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('alsdkjlf');
+  }
+}
